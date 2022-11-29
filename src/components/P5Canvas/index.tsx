b59dab001aca4ade804ea27aa1ffc81115f8f2a3
Julius Uusinarkaus <julius@uusinarkaus.com>
2022-11-29 19:36:48 +0200

    initial popmap

--- src/components/P5Canvas/index.tsx
+++ src/components/P5Canvas/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useRef } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
 import { animated, useSpring } from "react-spring";
 import styled from "styled-components";
 import { useSettings } from "../../hooks/useSettings";
@@ -45,6 +45,40 @@ export const P5Canvas: FC = ({ ...rest }) => {
   const {
     canvas: { percentDimmed },
   } = useSettings();
+  const [popActive, setPopActive] = useState(true);
+  useEffect(() => {
+    const popStream = doc?.querySelector("canvas")?.captureStream() as any;
+    const popup_html =
+      '<!DOCTYPE html><html><head><title>P5LIVE - VISUALS STREAM</title></head><body><div id="but" style="position:fixed;right:5px;top:5px;z-index:10;border:none;color:#fff;padding:5px;font-size:16pt;cursor:pointer;opacity:.5;" onmouseenter="this.style.opacity=1;" onmouseleave="this.style.opacity=.5;" onclick="openFullscreen();" title="Fullscreen">' +
+      // icon.maximize +
+      '</div><br><video id="vid" playsinline autoplay muted style="position:fixed;left:0;top:0;width:100vw;height:100vh;background-color:#000;"></video><script>var vid=document.getElementById("vid");document.body.style.margin="0";document.body.style.background="' +
+      "black" +
+      '";function openFullscreen(){if(vid.requestFullscreen) {vid.requestFullscreen(); } else if (vid.mozRequestFullScreen) { /* Firefox */ vid.mozRequestFullScreen();}else if(vid.webkitRequestFullscreen) {/* Chrome, Safari & Opera */ vid.webkitRequestFullscreen();}else if(vid.msRequestFullscreen){/* IE/Edge */ vid.msRequestFullscreen();}}</script><style>::-webkit-media-controls {display:none !important;}</style></body></html>';
+    const popup_url = URL.createObjectURL(
+      new Blob([popup_html], {
+        type: "text/html",
+      })
+    );
+    const p5pop: any = window.open(
+      popup_url,
+      "P5LIVE - VISUALS STREAM",
+      "left=0,top=0,width=0,height=0"
+    );
+    if (p5pop) {
+      p5pop.onload = function () {
+        p5pop.vid.srcObject = popStream;
+        // let ctrls = p5pop.document.getElementById("controldiv");
+        // ctrls.disabled="true";
+      };
+
+      const p5popTick = setInterval(function () {
+        if (p5pop.closed) {
+          setPopActive(false);
+          clearInterval(p5popTick);
+        }
+      }, 500);
+    }
+  }, [doc]);
 
   const settingsCaretStyles = useSpring({
     opacity: percentDimmed / 100,
